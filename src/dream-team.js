35a9f815d75f7b53def8d169e384d65ea707ead2
Ramil Khabibullin <khab1bullin92@yandex.ru>
2020-02-28 18:58:52 +0400

    feat: dream-team task solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,7 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports =  function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+   } else {
+    return members.filter(elem => typeof elem === "string").map(elem => elem.trim().slice(0,1).toUpperCase()).sort().join("")
+   }
 };
