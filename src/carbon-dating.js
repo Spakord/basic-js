682dbf294696107c8120f0ea8fc5bc1338ce7e9f
Ramil Khabibullin <khab1bullin92@yandex.ru>
2020-03-21 17:07:49 +0400

    feat: fix misprint

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -8,4 +8,4 @@ module.exports = function dateSample(sampleActivity) {
   const activity = parseFloat(sampleActivity);
   return isNaN(activity) ||  activity <= 0 || activity > 15 ? false : Math.ceil(Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
 };
-};
+
